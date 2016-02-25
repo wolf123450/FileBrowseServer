132b8f6851fc17e6c378a6742bf69235cd7eb62c
Clint <wolf123450@gmail.com>
2016-02-25 02:40:49 -0700

    uncomment

--- html/js/restructure.js
+++ html/js/restructure.js
@@ -4,9 +4,9 @@ var tools = ref.child("tools");
 
 var newTools = ref.child("newTools");
 
-// tools.on("child_added", function(snapshot){
-//     var updateObj = {};
-//     updateObj[snapshot.val().species] = {"users":{}};
-//
-//     newTools.child(snapshot.val().genus).child(snapshot.val().species).update({"users":"empty"});
-// })
+tools.on("child_added", function(snapshot){
+    var updateObj = {};
+    updateObj[snapshot.val().species] = {"users":{}};
+
+    newTools.child(snapshot.val().genus).child(snapshot.val().species).update({"users":"empty"});
+})
